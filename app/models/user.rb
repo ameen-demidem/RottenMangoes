d990e4e15ae56c510c76bd27ead0d1f6b3efb318
Ameen Demidem <ameen.demidem@gmail.com>
2016-08-17 16:05:42 -0400

    Add sign-up feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :firstname, :lastname, :email,
+    presence: true
+
+  validates :password,
+    presence: true, on: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817124335) do
+ActiveRecord::Schema.define(version: 20160817124919) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(version: 20160817124335) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "passowrd_digest"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
