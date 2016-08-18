71de7cd4761bbd74f7de30b5fafb9a668066165e
Ameen Demidem <ameen.demidem@gmail.com>
2016-08-18 15:48:20 -0400

    Add posting new movie reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+  def new
+    @movie = Movie.find_by(id: params[:movie_id])
+    if !@movie 
+      flash.alert = "Can find movie #{params[:movie_id]} to add a review for!"
+      redirect_to movies_path
+      return
+    end
+
+    @review = Review.new
+  end
+
+  def create
+    @movie = Movie.find_by(id: params[:movie_id])
+    if !@movie
+      flash.alert = "Can find movie #{params[:movie_id]} to add a review for!"
+      redirect_to movies_path
+      return
+    end
+
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.movie = @movie
+
+    if @review.save
+      redirect_to @movie, notice: "Review added successfully."
+    else
+      flash.now.alert = "Couldn't create a review for the movie"
+      render :new
+    end
+  end
+
+  protected
+
+  def review_params
+    params.require(:review).permit(:text, :rating_out_of_ten)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #  root "movies#index"
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
