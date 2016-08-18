70f83540dc0ccba294910dcfa357b7baad28c02f
Ameen Demidem <ameen.demidem@gmail.com>
2016-08-17 21:19:22 -0400

    Add logout functionality and login sign up links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 #  root "movies#index"
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
